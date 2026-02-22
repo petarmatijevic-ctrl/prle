65f111e72bfe3783ac51ba4694ec77b748a92239
petarmatijevic-ctrl <petarmatijevic02@gmail.com>
2026-02-22 17:32:06 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE='prle-v1';
+const URLS=['./','./index.html'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      if(r.ok){const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));}
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
